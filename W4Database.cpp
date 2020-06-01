364d29d2ecc4892d46e6c3ee957400daf8154983
sneilk <seleznevn@ya.ru>
2020-06-01 19:07:48 +0300

    added exceptions, fixed bugs with date, added bug cases

--- W4Database.cpp
+++ W4Database.cpp
@@ -6,6 +6,18 @@
  *
  *  Task:
  *  https://www.coursera.org/learn/c-plus-plus-white/programming/sO7Vq/final-naia-zadacha-kursa
+ *
+ *
+ *  Bugs:
+ *
+ *  1.
+ *  inp: Add 1---1-1 5
+ *  out: Wrong date format
+ *  inp: Add 1-69-42
+ *  out: Month value is invalid
+ *  inp: Add 1---1-1 5
+ *  out: Month value is invalid
+ *
  */
 
 #include <algorithm>
@@ -13,6 +25,7 @@
 #include <map>
 #include <sstream>
 #include <vector>
+#include <set>
 using namespace std;
 
 class Date{
@@ -23,32 +36,48 @@ public:
 
 	Date(int new_day, int new_month, int new_year){
 		if (new_month < 1 || new_month > 12) {
-			cout << "Month value is invalid: " << new_month << endl;
-
-			// ToDo: use exception
-
+			throw runtime_error("Month value is invalid: " + to_string(new_month));
 		}
 		int month_len = 31;
 		if ((new_day < 1) || (new_day > month_len)) {
-			cout << "Day value is invalid: " << new_day << endl;
-
-			// ToDo: use exception
-
+			throw runtime_error("Day value is invalid: " + to_string(new_day));
 		}
 		day = new_day;
 		month = new_month;
 		year = new_year;
 	}
-	  int GetYear() const{
-		  return year;
+	  string GetYear() const{
+		  int used_year = year;
+		  int size = 4;
+		  if (year < 0){
+			  used_year *= -1;
+			  size -= 1;
+		  }
+
+		  string s = to_string(used_year);
+		  for (int i =0; size - s.size(); i++) {
+			  s = '0' + s;
+		  }
+		  if (year < 0){
+			  s = '-' + s;
+		  }
+		  return s;
 	  }
 
-	  int GetMonth() const{
-		  return month;
+	  string GetMonth() const{
+		  string s = to_string(month);
+		  for (int i =0; 2 - s.size(); i++) {
+			  s = '0' + s;
+		  }
+		  return s;
 	  }
 
-	  int GetDay() const{
-		  return day;
+	  string GetDay() const{
+		  string s = to_string(day);
+		  for (int i =0; 2 - s.size(); i++) {
+			  s = '0' + s;
+		  }
+		  return s;
 	  }
 
 
@@ -73,30 +102,28 @@ bool operator<(const Date& lhs, const Date& rhs) {
 
 istream& operator>>(istream& in, Date& date){
 
-	string s, data;
+	string data, s;
 	int y, m, d;
 	char c1, c2;
 	in >> data;
 	istringstream input(data);
-	char c = input.peek();
-	if (c == '-') {
-		input.ignore(1);
-		input >> y ;
-		y *= -1;
+	input >> y >> c1 >> m >> c2 >> d >> s;
+
+	if (c1 != '-' || c2 != '-' || s != "") {
+//		cout << y << " " << m << " " << d << s << endl;
+//		cout << c1 << " " << c2 << endl;
+		throw runtime_error("Wrong date format: " + data);
 	} else {
-		input >> y;
-	}
-	input >> c1 >> m >> c2 >> d;
-
-	if (c1 != '-' || c2 != '-') {
-		cout << "Wrond date fomat: " << data;
-
-		// ToDo: use exception
-
-	} else {
-			date = Date(d, m, y);
+//		cout << y << " " << m << " " << d << s << endl;
+//		cout << c1 << " " << c2 << endl;
+		date = Date(d, m, y);
 	}
 	return in;
+}
+
+ostream& operator<<(ostream& out, const Date& date) {
+	out << date.GetYear() << '-' << date.GetMonth() << '-' << date.GetDay();
+	return out;
 }
 
 
@@ -105,14 +132,20 @@ public:
 
 	void AddEvent(const Date& date, const string& event) {
 		if (!IsInclude(events[date], event)) {
-			events[date].push_back(event);
-			sort(begin(events[date]), end(events[date]));
+			events[date].insert(event);
 		}
 	}
 
 	void DelEvent(const Date& date, const string& event){
 		if (events.count(date) && IsInclude(events[date], event)) {
-			cout << "Deleted successfully" << endl;
+		    auto search = events[date].find(event);
+		    if (search != events[date].end()){
+		    	events[date].erase(search);
+				cout << "Deleted successfully" << endl;
+		    }
+		    else {
+		    	cout << "Error in deletion of event" << endl;
+		    }
 		} else {
 			cout << "Event not found" << endl;
 		}
@@ -132,28 +165,25 @@ public:
 	void Find(const Date& date) const {
 		if (events.count(date)) {
 			for (const auto& i : events.at(date)) {
-				cout << i << " ";
+				cout << i << endl;
 			}
-			cout << endl;
 		}
 	}
 
 	void Print() const {
 		for (const auto& [key, val] : events) {
 			for (const auto i : val) {
-				cout << key.GetYear() << '-' <<
-						key.GetMonth() << '-' <<
-						key.GetDay() << " " << i << endl;
+				cout << key << " " << i << endl;
 			}
 		}
 
 	}
 
 private:
-	map<Date, vector<string>> events;
+	map<Date, set<string>> events;
 
 
-	bool IsInclude(const vector<string>& v, const string& event) const {
+	bool IsInclude(const set<string>& v, const string& event) const {
 		return (find(v.begin(), v.end(), event) != v.end());
 	}
 };
@@ -166,27 +196,44 @@ void DatabaseInteracrion(){
 		  istringstream input(command);
 		  if (input) {
 			  input >> command;
+
 			  if (command == "Add") {
 				  Date d;
 				  string event;
-				  input >> d >> event;
-				  db.AddEvent(d, event);
+				  try {
+					  input >> d >> event;
+					  db.AddEvent(d, event);
+				  }
+				  catch (runtime_error& ex) {
+					  cout << ex.what() << endl;
+				  }
 			  } else if (command == "Del") {
 				  Date d;
 				  string event;
-				  input >> d >> event;
-				  if (event.size()) {
-					  db.DelEvent(d, event);
-				  } else {
-					  db.DelDate(d);
+				  try {
+					  input >> d >> event;
+					  if (event.size()) {
+						  db.DelEvent(d, event);
+					  } else {
+						  db.DelDate(d);
+					  }
+				  }
+				  catch (runtime_error& ex) {
+					  cout << ex.what() << endl;
 				  }
 			  } else if (command == "Find") {
-				  Date d;
-  				  input >> d;
-  				  db.Find(d);
-
+				  try {
+					  Date d;
+					  input >> d;
+					  db.Find(d);
+				  }
+				  catch (runtime_error& ex) {
+					  cout << ex.what() << endl;
+				  }
 			  } else if (command == "Print") {
 				  db.Print();
+			  } else if (command != ""){
+				  cout << "Unknown command: " << command << endl;
 			  }
 		  }
 	  }
@@ -196,5 +243,4 @@ int main(){
 	DatabaseInteracrion();
 	return 0;
 }
-
 
